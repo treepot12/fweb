e538c5587d8ca866d09c6e978207bbf02c8f5931
win_hyeon_lim <61348734+treepot12@users.noreply.github.com>
2026-01-10 22:17:53 +0900

    수정3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {getFirestore} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMBsOREBROZmADYZRuM0lZwW3LOeuWe1k",
