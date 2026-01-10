149fee550abf8699068c5ebf610872843c6aafd9
win_hyeon_lim <61348734+treepot12@users.noreply.github.com>
2026-01-10 22:06:52 +0900

    수정

--- logo.js
+++ logo.js
@@ -1,15 +1,6 @@
-import { logout } from "./loged.js"
 window.logo = function (){
     location.href="https://treepot12.github.io/greatsune/";
 }
 window.clubjoin = function (){
     location.href="https://treepot12.github.io/greatsune/clubjoin.html";
-}
-window.login = function (){
-    location.href="https://treepot12.github.io/greatsune/login.html";
-}
-
-window.logot = function (){
-    logout();
-    location.href="https://treepot12.github.io/greatsune/";
 }
